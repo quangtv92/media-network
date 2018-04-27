2afe020b90ccb3722cdd28e1bb682b78ef121e9b
LongLH <d@dapps.me>
2018-04-27 14:31:15 +0700

    support partial templates and file-url

--- internals/webpack.dev.js
+++ internals/webpack.dev.js
@@ -1,32 +1,68 @@
 const path = require('path')
 
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const rootDir = path.resolve(__dirname, '..')
 
+console.log(rootDir)
+
 module.exports = {
   mode: 'development',
+  context: rootDir,
   entry: {
     app: [ path.join(rootDir, 'src/js/index.js') ]
   },
   output: {
     path: path.join(rootDir, 'public/assets'),
     publicPath: '/assets/',
-    filename: 'js/[name].bundle.js'
+    filename: 'js/[name].[hash:5].js'
   },
   plugins: [
+    new CleanWebpackPlugin([
+      path.join(rootDir, './public')
+    ], {
+      verbose: true,
+      watch: true,
+      allowExternal: true
+    }),
     new HtmlWebpackPlugin({
       inject: true,
-      template: path.join(rootDir, 'src/views/index.hbs'),
+      template: path.join(rootDir, 'src/views/index.html'),
       filename: path.join(rootDir, 'public/index.html'),
-      chunks: [ 'app' ]
+      chunks: [ 'app' ],
+      env: {
+        label: 'Hello world'
+      }
     })
   ],
+  resolve: {
+    modules: [ 'src', 'node_modules' ]
+  },
   module: {
     rules: [
       {
-        test: /\.hbs$/,
-        loader: 'handlebars-loader'
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              interpolate: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'img/[name].[hash:5].[ext]',
+              publicPath: '/assets/'
+            }
+          }
+        ]
       }
     ]
   }
